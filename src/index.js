a1070eb3706499ed922a4d357137aeea6aa5ccdb
VikinGSpb <imbot@inbox.ru>
2019-10-11 22:00:37 +0300

    feat: first algorithm

--- src/index.js
+++ src/index.js
@@ -38,7 +38,59 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    oldLength = arr.length;
+    for(let i = 0; i < oldLength + 1; i += 10)
+    {
+        let newArr = [];
+        for(let j = 0; j < 10; j++)
+        {
+            newArr.push(arr[i + j]);
+        }
+        arr.push(newArr);
+    }
+    arr.splice(0, oldLength);
+    let interimResult = [];
+    let result = [];
+    for(let i = 0; i < arr.length; i++)
+    {
+        if(interimResult[0] === ' '){
+            result.push(interimResult[0]);
+            interimResult.splice(0);
+        }
+        if(interimResult.length > 0)
+        {
+            for(let key in MORSE_TABLE)
+            {
+                if(key === interimResult.join(''))
+                {
+                    result.push(MORSE_TABLE[key]);
+                    break;
+                }
+            }
+            interimResult.splice(0);
+        }
+        for(let j = 9; j >= 0; j--)
+        {
+            if(arr[i][j] === '1')
+            {
+                interimResult.unshift('-');
+                j--;
+            } else {
+                if(arr[i][j-1] === '1')
+                {
+                    interimResult.unshift('.');
+                    j--;
+                } else {
+                   if(arr[i][j] === '*') {
+                       interimResult.unshift(' ');
+                       break;
+                   } else break;
+                }
+            }
+        }
+    }
+    return result.join('');
 }
 
 module.exports = {
